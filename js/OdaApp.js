001113b86ee9bd46fcc60d7aeb73db55388da8e2
Fabrice Rosito <fabrice.rosito@gmail.com>
2017-06-16 16:20:06 +0200

    update

--- js/OdaApp.js
+++ js/OdaApp.js
@@ -130,10 +130,10 @@
                     $.Oda.Log.trace("New message => type:" + e.data.messageType + ", from:" + e.data.userCode);
                     switch(e.data.messageType) {
                         case $.Oda.App.WebsocketMessageType.NEW_CONNECTION:
-                            $("#chat").append('<kureha-chat-notificaiton message="New user: '+e.data.userCode+' userCode="'+e.data.userCode+'"></kureha-chat-notificaiton>');
+                            $("#chat").append('<kureha-chat-notificaiton message="New user: '+e.data.userCode+'" userCode="'+e.data.userCode+'"></kureha-chat-notificaiton>');
                             break;
                         case $.Oda.App.WebsocketMessageType.CLOSE_CONNECTION:
-                            $("#chat").append('<kureha-chat-notificaiton message="User left: '+e.data.userCode+' userCode="'+e.data.userCode+'"></kureha-chat-notificaiton>');
+                            $("#chat").append('<kureha-chat-notificaiton message="User left: '+e.data.userCode+'" userCode="'+e.data.userCode+'"></kureha-chat-notificaiton>');
                             break;
                         case $.Oda.App.WebsocketMessageType.NEW_MESSAGE:
                             $("#chat").append('<kureha-chat-receive message="'+e.data.message+'" userCode="'+e.data.userCode+'"></kureha-chat-receive>');
